2de569d4820db2dff8d10bd44dc3098b318c5164
Galacticboreas <ivanovvolgdonbor@mail.ru>
2021-06-16 00:37:01 +0300

    delet add_staticfiles

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -98,8 +98,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'add_staticfiles')]
-
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
