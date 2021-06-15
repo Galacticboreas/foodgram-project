a9df41240cb9823af262b5a892e40c8bdda05b46
Galacticboreas <ivanovvolgdonbor@mail.ru>
2021-06-15 22:56:59 +0300

    add commit in error_func

--- foodgram/views.py
+++ foodgram/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 
 def error404(request, exception):
-    text = '404. Нет такой страницы :('
+    text = '404. Страница которую вы ищете не найдена.'
     caption = '404'
     return render(request, 'errorPage.html',
                   {'text': text, 'caption': caption}, status=404)
 
 
 def error500(request):
-    text = '500. Сервер не смог обработать ваш запрос :('
+    text = '500. Мы не смогли обработать ваш запрос.'
     caption = '500'
     return render(request, 'errorPage.html',
                   {'text': text, 'caption': caption}, status=500)
